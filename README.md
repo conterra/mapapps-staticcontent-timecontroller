8039d222f875971f3fda57bad3ee504a34be09cd
Matthias Stein <m.stein@conterra.de>
2016-09-15 11:36:26 +0200

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mapapps-staticcontent-timecontroller
+
+The StaticContent-TimeController Bundle deactivates the StaticContent widget after a certain period of time.
+
+### Sample App ###
+http://www.mapapps.de/mapapps/resources/apps/downloads_staticcontent_timecontroller/index.html
+
+### Define the mapapps remote base
+Before you can run the project you have to define the mapapps.remote.base property in the pom.xml-file:
+`<mapapps.remote.base>http://%YOURSERVER%/ct-mapapps-webapp-%VERSION%</mapapps.remote.base>`
+
+##### Other methods to to define the mapapps.remote.base property.
+1. Goal parameters
+`mvn install -Dmapapps.remote.base=http://%YOURSERVER%/ct-mapapps-webapp-%VERSION%`
+
+2. Build properties
+Change the mapapps.remote.base in the build.properties file and run:
+`mvn install -Denv=dev -Dlocal.configfile=%ABSOLUTEPATHTOPROJECTROOT%/build.properties`
+
+Installation Guide
+------------------
+
+1. First, you need to add the bundles "dn_staticcontenttimecontroller" to your app.
+2. After that, you can configure the bundle in the app.json.
+
+```
+"dn_staticcontenttimecontroller": {
+    "StaticContentTimeController": {
+        "time": 30000
+    }
+}
+```
---
+++ src/main/js/bundles/dn_staticcontenttimecontroller/StaticContentTimeController.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define([
+    "dojo/_base/declare",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/aspect"
+], function (declare, d_array, d_lang, d_aspect) {
+    return declare([], {
+        activate: function () {
+            var properties = this._properties;
+            d_aspect.after(this._contentviewer, "showContentInfo", d_lang.hitch({time: properties.time}, function (deferred, args) {
+                return deferred.then(d_lang.hitch({time: this.time}, function (response) {
+                    if (args[1].predefined) {
+                        var time = this.time;
+                        setTimeout(function () {
+                            response.window.close()
+                        }, time);
+                    }
+                    return response;
+                }));
+            }));
+        }
+    });
+});
